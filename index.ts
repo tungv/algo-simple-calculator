e9239043703496c4f419bc4edfb977227623e479
Tung Vu <me@tungv.com>
2024-08-24 12:34:26 +0700

    feat: add support for precedence
    Signed-off-by: Tung Vu <me@tungv.com>

--- index.ts
+++ index.ts
@@ -49,69 +49,77 @@ export default function calc(expression: string): number {
     value: currentToken,
   });
 
-  let currentNode = null as Partial<BinaryNode> | null;
+  // if there's only one token, it's a number
+  if (tokens.length === 1) {
+    return Number.parseInt(tokens[0].value, 10);
+  }
 
-  for (let i = 0; i < tokens.length; ++i) {
+  // if there are 2 tokens, it's invalid
+  if (tokens.length === 2) {
+    throw new Error("Invalid expression");
+  }
+
+  // take the first 3 tokens and create a binary node
+  const [n1, op, n2] = tokens;
+
+  let root: BinaryNode = {
+    type: "binary",
+    left: {
+      type: "number",
+      value: Number.parseInt(n1.value, 10),
+    },
+    operator: op.value,
+    right: {
+      type: "number",
+      value: Number.parseInt(n2.value, 10),
+    },
+  };
+
+  let currentPrecedence = getPrecedence(op.value);
+  let currentIncompleteNode: IncompleteBinaryNode = root;
+  let pendingReRoot = false;
+
+  // walk from the 4th token to the end
+  for (let i = 3; i < tokens.length; ++i) {
     const token = tokens[i];
 
-    if (currentNode === null) {
-      // looking for a number
-      if (token.type === "number") {
-        const value = Number.parseInt(token.value, 10);
-        currentNode = {
-          type: "binary",
-          left: {
-            type: "number",
-            value: value,
-          },
-        };
-      }
-    } else {
-      // looking for an operator
-      if (token.type === "operator") {
-        // if it already has an operator, it's invalid
-        if (currentNode.operator !== undefined) {
-          throw new Error("Invalid expression");
-        }
+    switch (token.type) {
+      case "operator": {
+        const operator = token.value;
+        const precedence = getPrecedence(operator);
 
-        if (currentNode.left === undefined) {
-          throw new Error("Invalid expression");
+        if (precedence > currentPrecedence) {
+          // add down
+          currentIncompleteNode = addDown(root, operator);
+        } else {
+          // add up
+          currentIncompleteNode = addUp(root, operator);
+          pendingReRoot = true;
         }
-
-        currentNode.operator = token.value;
+        currentPrecedence = precedence;
+        break;
       }
 
-      // looking for a number to complete the binary node
-      if (token.type === "number") {
-        currentNode.right = {
+      case "number": {
+        const leaf: LeafNode = {
           type: "number",
           value: Number.parseInt(token.value, 10),
         };
 
-        // this should be complete
-        validateBinaryNode(currentNode);
-
-        // if this is the last token, we're done
-        if (i === tokens.length - 1) {
-          break;
+        currentIncompleteNode.right = leaf;
+        if (pendingReRoot) {
+          root = currentIncompleteNode as BinaryNode;
+          pendingReRoot = false;
         }
-
-        // move on
-        currentNode = {
-          type: "binary",
-          left: currentNode as BinaryNode,
-        };
+        break;
       }
+
+      default:
+        throw new Error("Invalid token");
     }
   }
 
-  if (currentNode === null) {
-    throw new Error("Invalid expression");
-  }
-
-  // console.log(currentNode);
-
-  return getValue(currentNode as BinaryNode);
+  return getValue(root);
 }
 
 function validateBinaryNode(
@@ -142,7 +150,7 @@ function validateBinaryNode(
   }
 }
 
-function getValue(node: BinaryNode | LeaveNode): number {
+function getValue(node: BinaryNode | LeafNode): number {
   if (node.type === "number") {
     return node.value;
   }
@@ -166,14 +174,87 @@ function getValue(node: BinaryNode | LeaveNode): number {
   return 0;
 }
 
-interface LeaveNode {
+interface LeafNode {
   type: "number";
   value: number;
 }
 
+interface IncompleteBinaryNode {
+  type: "binary";
+  left: BinaryNode | LeafNode;
+  operator?: string;
+  right?: IncompleteBinaryNode | LeafNode;
+}
+
 interface BinaryNode {
   type: "binary";
-  left: BinaryNode | LeaveNode;
+  left: BinaryNode | LeafNode;
   operator: string;
-  right: BinaryNode | LeaveNode;
+  right: BinaryNode | LeafNode;
+}
+
+/* add a new node to the right of tree and return the tree and the new node
+ * when we have a new operation that is higher or equal precedence than the current operation
+ * we will always add it the rightmost node and replace the right node with a binary node
+ * assumption: the rightmost node is always a leaf node
+ *
+ *    1 + 2 * 3
+ *
+ *    +      ->   +
+ *  1   2       1   *
+ *                2   3
+ **/
+function addDown(tree: BinaryNode, operator: string): IncompleteBinaryNode {
+  let parentOfTheRightmostNode = tree;
+  let rightmostNode = tree.right;
+
+  while (rightmostNode.type === "binary") {
+    parentOfTheRightmostNode = rightmostNode;
+    rightmostNode = rightmostNode.right;
+  }
+
+  // replace the rightmost node with a binary node
+  const newRight: IncompleteBinaryNode = {
+    type: "binary",
+    left: rightmostNode,
+    operator: operator,
+  };
+
+  (parentOfTheRightmostNode as IncompleteBinaryNode).right = newRight;
+
+  return newRight;
+}
+
+/* add a new node the right of the tree and return the new tree
+ * when we have a new operation that is lower precedence than the current operation
+ * we will always make a new root and assign the current tree to its left node
+ *
+ *
+ *   1 * 2 + 3
+ *
+ *   *       ->        +
+ * 1   2            *     3
+ *                1   2
+ */
+function addUp(tree: BinaryNode, operator: string): IncompleteBinaryNode {
+  return {
+    type: "binary",
+    left: tree,
+    operator: operator,
+  };
+}
+
+function getPrecedence(operator: string): number {
+  switch (operator) {
+    case "+":
+    case "-":
+      return 1;
+
+    case "*":
+    case "/":
+      return 2;
+
+    default:
+      throw new Error("Invalid operator");
+  }
 }
